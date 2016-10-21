d5e1edb881485c9e216b74ee1d55df4079743b2c
Purvi <purvi194@gmail.com>
2016-10-21 18:02:10 +0530

    Beautification done and some minor changes

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,27 +1,17 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
-
 // $("#main").append("Purvi Bansal");
-
 // var awesomeThoughts="I am Purvi and I am awesome!!!"
 // console.log(awesomeThoughts);
-
 // var funThoughts= awesomeThoughts.replace("awesome","fun");
 // console.log(funThoughts);
-
 // $("#main").append(funThoughts);
 // var name="Purvi Bansal";
-
-
 // var role="Front-End Web Developer"
 // var formattedRole= HTMLheaderRole.replace("%data%",role);
-
 // $("#header").prepend(formattedRole);
 // $("#header").prepend(formattedName);
-
-
-
 // $("#main").append(bio.name);
 // $("#main").append(bio.role);
 // $("#main").append(bio.contacts.mobile);
@@ -31,259 +21,242 @@ This is empty on purpose! Your code to build the resume will go here.
 // $("#main").append(bio.bioPic);
 // $("#main").append(bio.welcomeMessage);
 // $("#main").append(bio.skills);
-
 // bio["work"]= {
 // 	"position" : "Trainee",
 // 	"employer" : "Google",
 // 	"years" : "1",
 // 	"city" : "San Francisco"
 // }
-
-
-var skills= ["Programming","Content Writing","Reading"]
+var skills = ["Programming", "Content Writing", "Reading"]
 
 var bio = {
-	"name" : "Purvi Bansal",
-	"role" : "Front-End Web Developer",
-	"welcomeMessage" : "Lets Work Together..... And learn and grow together..",
-	"bioPic" : "images/fry.jpg",
-	"contacts" : {
-		"mobile" : "123-456-7890",
-		"email" : "purvi194@gmail.com",
-		"github" : "purvi194",
-		"location" : "Indore, Madhya Pradesh, India"
-		},
-	"skills" :  skills
+    "name": "Purvi Bansal",
+    "role": "Front-End Web Developer",
+    "welcomeMessage": "Lets work together..... Learn and grow together..",
+    "bioPic": "images/fry.jpg",
+    "contacts": {
+        "mobile": "123-456-7890",
+        "email": "purvi194@gmail.com",
+        "github": "purvi194",
+        "location": "Indore, Madhya Pradesh, India"
+    },
+    "skills": skills
 };
 
-bio.display=function(){
-	var formattedName= HTMLheaderName.replace("%data%",bio.name);
-	var formattedRole= HTMLheaderRole.replace("%data%",bio.role);
-	var formattedMobile= HTMLmobile.replace("%data%",bio.contacts.mobile);
-	var formattedMEmail= HTMLemail.replace("%data%",bio.contacts.email);
-	var formattedGithub= HTMLgithub.replace("%data%",bio.contacts.github);
-	var formattedLocation= HTMLlocation.replace("%data%",bio.contacts.location);
-	var formattedBiopic= HTMLbioPic.replace("%data%",bio.bioPic);
-	var formattedWelcomeMsg= HTMLwelcomeMsg.replace("%data%",bio.welcomeMessage);
+bio.display = function() {
+    var formattedName = HTMLheaderName.replace("%data%", bio.name);
+    var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+    var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
+    var formattedMEmail = HTMLemail.replace("%data%", bio.contacts.email);
+    var formattedGithub = HTMLgithub.replace("%data%", bio.contacts.github);
+    var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
+    var formattedBiopic = HTMLbioPic.replace("%data%", bio.bioPic);
+    var formattedWelcomeMsg = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
 
-	$("#header").prepend(formattedRole);
-	$("#header").prepend(formattedName);
+    $("#header").prepend(formattedBiopic);
+    $("#header").prepend(formattedWelcomeMsg);
+    $("#header").prepend(formattedRole);
+    $("#header").prepend(formattedName);
 
-	$("#topContacts").append(formattedMobile);
-	$("#topContacts").append(formattedMEmail);
-	$("#topContacts").append(formattedGithub);
-	$("#topContacts").append(formattedLocation);
+    $("#topContacts").append(formattedMobile);
+    $("#topContacts").append(formattedMEmail);
+    $("#topContacts").append(formattedGithub);
+    $("#topContacts").append(formattedLocation);
 
-	$("#header").append(formattedBiopic);
-	$("#header").append(formattedWelcomeMsg);
+    $("#footerContacts").append(formattedMobile);
+    $("#footerContacts").append(formattedMEmail);
+    $("#footerContacts").append(formattedGithub);
+    $("#footerContacts").append(formattedLocation);
 
-	if(bio.skills.length > 0)
-	{
-		$("#header").append(HTMLskillsStart);
+    if (bio.skills.length > 0) {
+        $("#header").append(HTMLskillsStart);
 
-		var formattedSkills= HTMLskills.replace("%data%",bio.skills[0]);
-		$("#skills").append(formattedSkills);
+        var formattedSkills = HTMLskills.replace("%data%", bio.skills[0]);
+        $("#skills").append(formattedSkills);
 
-		formattedSkills= HTMLskills.replace("%data%",bio.skills[1]);
-		$("#skills").append(formattedSkills);
+        formattedSkills = HTMLskills.replace("%data%", bio.skills[1]);
+        $("#skills").append(formattedSkills);
 
-		formattedSkills= HTMLskills.replace("%data%",bio.skills[2]);
-		$("#skills").append(formattedSkills);
-	}
+        formattedSkills = HTMLskills.replace("%data%", bio.skills[2]);
+        $("#skills").append(formattedSkills);
+    }
 
 };
 
 bio.display();
 
 
-var work= {
-		"jobs":	[
-		 {
-				"employer" : "SVVIMUN",
-				"title" : "Web Designer",
-				"location" : "Indore, India",
-				"dates" : "August 2016-October 2016",
-				"description" : "Designing the front end for the website for the registration"
-			},
-			{
-				"employer" : "Technomech Engineers",
-				"title" : "DBA",
-				"location" : "Indore, India",
-				"dates" : "August 2014-August 2016",
-				"description" : "Maintanance and updation of the database"
-			}
-		]
-		}
+var work = {
+    "jobs": [{
+        "employer": "SVVIMUN",
+        "title": "Web Designer",
+        "location": "Indore, India",
+        "dates": "August 2016-October 2016",
+        "description": "Designing the front end for the website for the registration"
+    }, {
+        "employer": "Technomech Engineers",
+        "title": "DBA",
+        "location": "Indore, India",
+        "dates": "August 2014-August 2016",
+        "description": "Maintanance and updation of the database"
+    }]
+}
 
-work.display=function() {
-work.jobs.forEach(function(job) {
+work.display = function() {
+    work.jobs.forEach(function(job) {
 
-	$("#workExperience").append(HTMLworkStart);
+        $("#workExperience").append(HTMLworkStart);
 
-	var formattedWorkemployer= HTMLworkEmployer.replace("%data%",job.employer);
-	var formattedWorktitle=HTMLworkTitle.replace("%data%",job.title);
-	var wrkconcat=formattedWorkemployer+formattedWorktitle;
-	$(".work-entry:last").append(wrkconcat);
+        var formattedWorkemployer = HTMLworkEmployer.replace("%data%", job.employer);
+        var formattedWorktitle = HTMLworkTitle.replace("%data%", job.title);
+        var wrkconcat = formattedWorkemployer + formattedWorktitle;
+        $(".work-entry:last").append(wrkconcat);
 
-	var formattedDates = HTMLworkDates.replace("%data%",job.dates);
-	$(".work-entry:last").append(formattedDates);
+        var formattedDates = HTMLworkDates.replace("%data%", job.dates);
+        $(".work-entry:last").append(formattedDates);
 
-	var formattedworkLocation = HTMLworkLocation.replace("%data%",job.location);
-	$(".work-entry:last").append(formattedworkLocation);
+        var formattedworkLocation = HTMLworkLocation.replace("%data%", job.location);
+        $(".work-entry:last").append(formattedworkLocation);
 
-	var formattedDescription= HTMLworkDescription.replace("%data%",job.description);
-	$(".work-entry:last").append(formattedDescription);
-});
+        var formattedDescription = HTMLworkDescription.replace("%data%", job.description);
+        $(".work-entry:last").append(formattedDescription);
+    });
 };
 
 work.display();
 
 $(document).click(function(loc) {
-	var x = loc.pageX;
-	var y = loc.pageY;
+    var x = loc.pageX;
+    var y = loc.pageY;
 
-  logClicks(x,y);
+    logClicks(x, y);
 });
 
 // INTERNATIONALIZE NAME 
 
 $("#main").append(internationalizeButton);
 
-function inName(name)
-{
-	name=name.trim();
-	console.log(name);
-	var nameArray= name.split(" ");
-	iName= nameArray[0].slice(0,1).toUpperCase()+nameArray[0].slice(1).toLowerCase()+" "+nameArray[1].toUpperCase();
-	return iName;
+function inName(name) {
+    name = name.trim();
+    console.log(name);
+    var nameArray = name.split(" ");
+    iName = nameArray[0].slice(0, 1).toUpperCase() + nameArray[0].slice(1).toLowerCase() + " " + nameArray[1].toUpperCase();
+    return iName;
 }
 
 
 
-var education= {
-	"schools": 
-		[
-			{
-				"name" : "Queens College",
-				"location" : "Indore, Madhya Pradesh, India",
-				"dates" : "2001-12",
-				"major": "NA",
-				"degree": "Matriculate",
-				"url": "http://www.queenscollegeindore.org"
-				
-			},
-			{
-				"name": "Poornima Institute of Engineering and Technology",		
-				"location": "Jaipur, Rajasthan, India",
-				"dates": "2012-16",
-				"major": "Computer Science",
-				"degree": "Bachelors in Technology(Computer Science)",
-				"url": "http://www.poornima.org"
-			}
-		],
-			
-	"online_courses":
-		[
-			{
-				"title": "Front-End Web Developer",
-				"school": "Udacity",
-				"dates": "2016-present",
-				"url": "https://www.udacity.com/course/front-end-web-developer-nanodegree--nd001?v=fe3"
-			}
-		]
+var education = {
+    "schools": [{
+        "name": "Queens College",
+        "location": "Indore, Madhya Pradesh, India",
+        "dates": "2001-12",
+        "major": "NA",
+        "degree": "Matriculate",
+        "url": "http://www.queenscollegeindore.org"
+
+    }, {
+        "name": "Poornima Institute of Engineering and Technology",
+        "location": "Jaipur, Rajasthan, India",
+        "dates": "2012-16",
+        "major": "Computer Science",
+        "degree": "Bachelors in Technology(Computer Science)",
+        "url": "http://www.poornima.org"
+    }],
+
+    "online_courses": [{
+        "title": "Front-End Web Developer",
+        "school": "Udacity",
+        "dates": "2016-present",
+        "url": "https://www.udacity.com/course/front-end-web-developer-nanodegree--nd001?v=fe3"
+    }]
 };
 
-education.display= function() {
+education.display = function() {
 
-	education.schools.forEach(function(schools) {
+    education.schools.forEach(function(schools) {
 
-		$("#education").append(HTMLschoolStart);
+        $("#education").append(HTMLschoolStart);
 
-		formattedSchoolname= HTMLschoolName.replace("%data%",schools.name);
-		formattedSchoolLoc= HTMLschoolLocation.replace("%data%",schools.location);
-		formattedSchooldates= HTMLschoolDates.replace("%data%",schools.dates);
-		formattedSchoolmajor= HTMLschoolMajor.replace("%data%",schools.major);
-		formattedSchooldegree= HTMLschoolDegree.replace("%data%", schools.degree);
+        formattedSchoolname = HTMLschoolName.replace("%data%", schools.name);
+        formattedSchoolLoc = HTMLschoolLocation.replace("%data%", schools.location);
+        formattedSchooldates = HTMLschoolDates.replace("%data%", schools.dates);
+        formattedSchoolmajor = HTMLschoolMajor.replace("%data%", schools.major);
+        formattedSchooldegree = HTMLschoolDegree.replace("%data%", schools.degree);
 
-		$(".education-entry:last").append(formattedSchoolname);
-		$(".education-entry:last").append(formattedSchooldegree);
-		$(".education-entry:last").append(formattedSchooldates);
-		$(".education-entry:last").append(formattedSchoolLoc);
-		$(".education-entry:last").append(formattedSchoolmajor);
+        $(".education-entry:last").append(formattedSchoolname);
+        $(".education-entry:last").append(formattedSchooldegree);
+        $(".education-entry:last").append(formattedSchooldates);
+        $(".education-entry:last").append(formattedSchoolLoc);
+        $(".education-entry:last").append(formattedSchoolmajor);
 
-	});
+    });
 
-	$(".education-entry:last").append(HTMLonlineClasses);
+    $(".education-entry:last").append(HTMLonlineClasses);
 
-	education.online_courses.forEach(function(courses) {
+    education.online_courses.forEach(function(courses) {
 
-		formattedCoursetitle= HTMLonlineTitle.replace("%data%",courses.title);
-		formattedCourseschool= HTMLonlineSchool.replace("%data%",courses.school);
-		formattedCoursedates= HTMLonlineDates.replace("%data%",courses.dates);
-		formattedCourseURL= HTMLonlineURL.replace("%data%",courses.url);
+        formattedCoursetitle = HTMLonlineTitle.replace("%data%", courses.title);
+        formattedCourseschool = HTMLonlineSchool.replace("%data%", courses.school);
+        formattedCoursedates = HTMLonlineDates.replace("%data%", courses.dates);
+        formattedCourseURL = HTMLonlineURL.replace("%data%", courses.url);
 
-		$(".education-entry:last").append(formattedCoursetitle);
-		$(".education-entry:last").append(formattedCourseschool);
-		$(".education-entry:last").append(formattedCoursedates);
-		$(".education-entry:last").append(formattedCourseURL);
-	});
+        $(".education-entry:last").append(formattedCoursetitle);
+        $(".education-entry:last").append(formattedCourseschool);
+        $(".education-entry:last").append(formattedCoursedates);
+        $(".education-entry:last").append(formattedCourseURL);
+    });
 
 }
 
 education.display();
 
-var projects={
+var projects = {
 
-	"projects" : [
-		{
-			"title": "College ERP",
-			"dates": "2015-16",
-			"description": "An ERP for college which enables management of all the departments easier and central.",
-			"image":"images/erp.jpg"
+    "projects": [{
+        "title": "College ERP",
+        "dates": "2015-16",
+        "description": "An ERP for college which enables management of all the departments easier and central.",
+        "image": "images/erp.jpg"
 
-		},
-		{
-			"title": "Droid Control",
-			"dates": "2016",
-			"description": "An android application which enables control of a pick and place robot using bluetooth connection.",
-			"image":"images/droid.jpg"
-		},
-		{
-			"title": "Call Blocker",
-			"dates": "2015",
-			"description": "An android application which is helpful in blocking unwanted or unknown calls,",
-			"image":"images/blocker.jpg"
-		},
-		{
-			"title": "Lecture Distribution System",
-			"dates": "2014",
-			"description": "A .net based project which is used to distribute the notes provided by the professors.",
-			"image":"images/lectures.jpg"
-		}		
-	]
+    }, {
+        "title": "Droid Control",
+        "dates": "2016",
+        "description": "An android application which enables control of a pick and place robot using bluetooth connection.",
+        "image": "images/droid.jpg"
+    }, {
+        "title": "Call Blocker",
+        "dates": "2015",
+        "description": "An android application which is helpful in blocking unwanted or unknown calls,",
+        "image": "images/blocker.jpg"
+    }, {
+        "title": "Lecture Distribution System",
+        "dates": "2014",
+        "description": "A .net based project which is used to distribute the notes provided by the professors.",
+        "image": "images/lectures.jpg"
+    }]
 }
 
-projects.display= function() {
+projects.display = function() {
 
-	projects.projects.forEach(function(list) {
+    projects.projects.forEach(function(list) {
 
-			$("#projects").append(HTMLprojectStart);
+        $("#projects").append(HTMLprojectStart);
 
-			var formattedprojectTitle= HTMLprojectTitle.replace("%data%",list.title);
-			var formattedprojectDates= HTMLprojectDates.replace("%data%",list.dates);
-			var formattedprojectDesc= HTMLprojectDescription.replace("%data%",list.description);
-			var formattedprojectImage= HTMLprojectImage.replace("%data%",list.image);
+        var formattedprojectTitle = HTMLprojectTitle.replace("%data%", list.title);
+        var formattedprojectDates = HTMLprojectDates.replace("%data%", list.dates);
+        var formattedprojectDesc = HTMLprojectDescription.replace("%data%", list.description);
+        var formattedprojectImage = HTMLprojectImage.replace("%data%", list.image);
 
-			$(".project-entry:last").append(formattedprojectTitle);
-			$(".project-entry:last").append(formattedprojectDates);
-			$(".project-entry:last").append(formattedprojectDesc);
-			$(".project-entry:last").append(formattedprojectImage);
+        $(".project-entry:last").append(formattedprojectTitle);
+        $(".project-entry:last").append(formattedprojectDates);
+        $(".project-entry:last").append(formattedprojectDesc);
+        $(".project-entry:last").append(formattedprojectImage);
 
-		});
+    });
 }
 
 projects.display();
 
 
 $("#mapDiv").append(googleMap);
-
